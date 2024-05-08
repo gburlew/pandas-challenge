21f163c7fc461d3dc4f0bd59d44536bf13cb980f
gburlew <164833372+gburlew@users.noreply.github.com>
2024-05-07 19:12:50 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import json
+from pathlib import Path
+
+locations_age_csv = Path("locations-age.csv")
+vaccinations_age_csv = Path("vaccinations-by-age-group.csv")
+vaccinations_manufacturer_csv = Path("vaccinations-by-manufacturer.csv")
